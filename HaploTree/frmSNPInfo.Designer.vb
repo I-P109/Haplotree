6ddd8f4b8d5b0c0dc157bb9446bcf02dc8fffdca
Darin Flansburg <darin.flansburg@s-aga.org>
2018-01-03 17:27:03 -0600

    New test... do you see the frmSNPInfo.Designer.vb?

---
+++ HaploTree/frmSNPInfo.Designer.vb
@@ -0,0 +1,131 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmSNPInfo
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblAlt = New System.Windows.Forms.Label()
+        Me.lblRef = New System.Windows.Forms.Label()
+        Me.lblPosition = New System.Windows.Forms.Label()
+        Me.txtAlt = New System.Windows.Forms.TextBox()
+        Me.txtRef = New System.Windows.Forms.TextBox()
+        Me.txtPosition = New System.Windows.Forms.TextBox()
+        Me.btnOK = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblAlt
+        '
+        Me.lblAlt.AutoSize = True
+        Me.lblAlt.Location = New System.Drawing.Point(69, 82)
+        Me.lblAlt.Name = "lblAlt"
+        Me.lblAlt.Size = New System.Drawing.Size(19, 13)
+        Me.lblAlt.TabIndex = 0
+        Me.lblAlt.Text = "Alt"
+        '
+        'lblRef
+        '
+        Me.lblRef.AutoSize = True
+        Me.lblRef.Location = New System.Drawing.Point(64, 51)
+        Me.lblRef.Name = "lblRef"
+        Me.lblRef.Size = New System.Drawing.Size(24, 13)
+        Me.lblRef.TabIndex = 1
+        Me.lblRef.Text = "Ref"
+        '
+        'lblPosition
+        '
+        Me.lblPosition.AutoSize = True
+        Me.lblPosition.Location = New System.Drawing.Point(44, 22)
+        Me.lblPosition.Name = "lblPosition"
+        Me.lblPosition.Size = New System.Drawing.Size(44, 13)
+        Me.lblPosition.TabIndex = 2
+        Me.lblPosition.Text = "Position"
+        '
+        'txtAlt
+        '
+        Me.txtAlt.Location = New System.Drawing.Point(103, 79)
+        Me.txtAlt.Name = "txtAlt"
+        Me.txtAlt.Size = New System.Drawing.Size(100, 20)
+        Me.txtAlt.TabIndex = 3
+        '
+        'txtRef
+        '
+        Me.txtRef.Location = New System.Drawing.Point(103, 48)
+        Me.txtRef.Name = "txtRef"
+        Me.txtRef.Size = New System.Drawing.Size(100, 20)
+        Me.txtRef.TabIndex = 4
+        '
+        'txtPosition
+        '
+        Me.txtPosition.Location = New System.Drawing.Point(103, 22)
+        Me.txtPosition.Name = "txtPosition"
+        Me.txtPosition.Size = New System.Drawing.Size(100, 20)
+        Me.txtPosition.TabIndex = 5
+        '
+        'btnOK
+        '
+        Me.btnOK.Location = New System.Drawing.Point(152, 134)
+        Me.btnOK.Name = "btnOK"
+        Me.btnOK.Size = New System.Drawing.Size(75, 23)
+        Me.btnOK.TabIndex = 6
+        Me.btnOK.Text = "OK"
+        Me.btnOK.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Location = New System.Drawing.Point(31, 134)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(75, 23)
+        Me.btnCancel.TabIndex = 7
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'frmSNPInfo
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 203)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.btnOK)
+        Me.Controls.Add(Me.txtPosition)
+        Me.Controls.Add(Me.txtRef)
+        Me.Controls.Add(Me.txtAlt)
+        Me.Controls.Add(Me.lblPosition)
+        Me.Controls.Add(Me.lblRef)
+        Me.Controls.Add(Me.lblAlt)
+        Me.Name = "frmSNPInfo"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "SNP Info"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblAlt As Label
+    Friend WithEvents lblRef As Label
+    Friend WithEvents lblPosition As Label
+    Friend WithEvents txtAlt As TextBox
+    Friend WithEvents txtRef As TextBox
+    Friend WithEvents txtPosition As TextBox
+    Friend WithEvents btnOK As Button
+    Friend WithEvents btnCancel As Button
+End Class
